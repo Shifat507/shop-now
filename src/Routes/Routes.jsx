e20ef44da26e14275d762f5a4eb170ee925c3bf5
Shifat507 <ferdoushasanshifat@gmail.com>
2025-05-23 21:19:47 +0600

    added Navbar, Banner and footer

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
---
+++ src/components/Banner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import slider1 from '../assets/Banner/Banner1.jpg'
+import slider2 from '../assets/Banner/Banner2.jpg'
+import slider3 from '../assets/Banner/Banner3.jpg'
+import slider4 from '../assets/Banner/Banner4.jpg'
+const Banner = () => {
+    return (
+        <div>
+            <Carousel>
+                <div>
+                    <img src={slider1} />
+                </div>
+                <div>
+                    <img src={slider2} />          
+                </div>
+                <div>
+                    <img src={slider3} />            
+                </div>
+                <div>
+                    <img src={slider4} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
